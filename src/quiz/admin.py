17bc6e35dcf24c940aa09a089ca6427adbf2cacb
SemihDurmus <semihdurmus@yahoo.com>
2021-01-16 12:11:32 +0100

    Add : nested_admin

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
+import nested_admin
+
 from .models import Category, Quiz, Question, Answer
 
+
+class AnswerInline(nested_admin.NestedTabularInline):  # make this an inline
+    model = Answer
+
+
+class QuestionInline(nested_admin.NestedTabularInline):
+    model = Question
+    inlines = [AnswerInline]
+
+
+class QuizAdmin(nested_admin.NestedModelAdmin):
+    model = Quiz
+    inlines = [QuestionInline]
+
+
 admin.site.register(Category)
-admin.site.register(Quiz)
+# We have to overwrite because we added sth new to admin in QuizAdmin
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question)
 admin.site.register(Answer)
--- src/quiz/models.py
+++ src/quiz/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
-    def _str__(self):
+    def __str__(self):
         return self.name
+
     class Meta:
         verbose_name_plural = "Categories"
 
+
 class Quiz(models.Model):
     title = models.CharField(max_length=50, verbose_name="Quiz Title")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) #if Category is deleted Quizzes are also deleted
+    # if Category is deleted Quizzes are also deleted
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
 
-    def _str__(self):
+    def __str__(self):
         return self.title
 
     class Meta:
@@ -22,8 +26,10 @@ class Quiz(models.Model):
 
 class Update(models.Model):
     updated = models.DateTimeField(auto_now_add=True)
+
     class Meta:
-        abstract = True #This means it is not migrated to DB. Only to be inherited to other databases
+        # This means it is not migrated to DB. Only to be inherited to other databases
+        abstract = True
 
 
 class Question(Update):
@@ -34,19 +40,22 @@ class Question(Update):
         (2, "Advanced"),
     )
 
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE) #If Quiz is deleted Questions are also deleted
+    # If Quiz is deleted Questions are also deleted
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     title = models.CharField(max_length=500, verbose_name="question")
     difficulty = models.IntegerField(choices=SCALE)
     date_created = models.DateTimeField(auto_now_add=True)
-    #updated = models.DateTimeField(auto_now_add=True) #We ainherit Update class instead of using models.Model as parameter
+    # updated = models.DateTimeField(auto_now_add=True) #We ainherit Update class instead of using models.Model as parameter
 
-    def _str__(self):
+    def __str__(self):
         return self.title
+
 
 class Answer(Update):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=200)
     is_right = models.BooleanField(default=False)
     #updated = models.DateTimeField(auto_now_add=True)
+
     def _str__(self):
         return self.answer_text
